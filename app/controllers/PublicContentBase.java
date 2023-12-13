04103c2dddbb23f33ad579d666ef345eb4b855c2
javi-ag <153739397+javi-ag@users.noreply.github.com>
2023-12-13 23:08:47 +0100

    Update PublicContentBase.java

--- app/controllers/PublicContentBase.java
+++ app/controllers/PublicContentBase.java
@@ -11,9 +11,16 @@ public class PublicContentBase extends Controller {
         render();
     }
 
-    public static void processRegister(String username, String password, String passwordCheck, String type){
-        User u = new User(username, HashUtils.getMd5(password), type, -1);
+    //Se modifica el método de registro para adecuarse al algoritmo PBKDF2
+    public static void processRegister(String username, String password, String passwordCheck, String type) {
+
+            // Genera un salt aleatorio
+            String salt = HashUtils.generateRandomSalt();
+            // Crea el usuario con el hash de la contraseña y el salt aleatorio
+            String hashedPassword = HashUtils.getPBKDF2Hash(password, salt);
+            User u = new User(username, hashedPassword, salt, type, -1);
         u.save();
+   
         registerComplete();
     }
 
