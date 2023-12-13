7f30b6436b6bb9aff4541e69979b63614cbbbf8c
javi-ag <153739397+javi-ag@users.noreply.github.com>
2023-12-13 23:11:21 +0100

    Update HashUtils.java

--- app/helpers/HashUtils.java
+++ app/helpers/HashUtils.java
@@ -1,23 +1,46 @@
 package helpers;
 
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+import java.security.SecureRandom;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.PBEKeySpec;
 
 public class HashUtils {
 
-    public static String getMd5(String s){
+    // Método para obtener el hash PBKDF2 con salt
+    public static String getPBKDF2Hash(String password, String salt) {
+        int iterations = 100000; // Número de iteraciones
+        int keyLength = 256; // Longitud de la clave
+        char[] passwordChars = password.toCharArray();
+        byte[] saltBytes = Base64.getDecoder().decode(salt);
+
         try {
-            MessageDigest m = MessageDigest.getInstance("MD5");
-            m.reset();
-            m.update(s.getBytes());
-            byte[] digest = m.digest();
-            BigInteger bigInt = new BigInteger(1,digest);
-            String hashtext = bigInt.toString(16);
-            while(hashtext.length() < 32){
-                hashtext = "0" + hashtext;
-            }
-        } catch (Exception e) {}
-        return s;
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            PBEKeySpec spec = new PBEKeySpec(passwordChars, saltBytes, iterations, keyLength);
+            SecretKey key = skf.generateSecret(spec);
+            byte[] hashBytes = key.getEncoded();
+            return Base64.getEncoder().encodeToString(hashBytes);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
+
+    // Método para generar un salt aleatorio
+    public static String generateRandomSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] saltBytes = new byte[16]; // La longitud del salt en bytes (puedes ajustar esto según tus necesidades)
+        random.nextBytes(saltBytes);
+        return Base64.getEncoder().encodeToString(saltBytes);
+    }
+
+    // Método para verificar el hash PBKDF2 con salt
+    public static boolean checkPBKDF2Hash(String password, String hashedPassword, String salt) {
+        return hashedPassword.equals(getPBKDF2Hash(password, salt));
+}
 }
